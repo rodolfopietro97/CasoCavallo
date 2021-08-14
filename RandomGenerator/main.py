2d870d918a31945d0bd9cc6fe47e344a001d7abc
rodolfopietro97 <rodolfopietro97@gmail.com>
2021-08-14 22:08:05 +0200

    added microservices

--- RandomGenerator/main.py
+++ RandomGenerator/main.py
@@ -1,5 +1,22 @@
 from Sources.Internal.DevUrandomRandomSource import DevUrandomRandomSource
+import redis
+
+# Redis server informations
+REDIS_SERVER = {
+    'host': '1.0.0.10',
+    'port': 6379
+}
 
 if __name__ == '__main__':
-    randomSource = DevUrandomRandomSource()
-    print(randomSource.get_random_bytes(4))
+    """
+    Main method
+    """
+
+    # Init local random source
+    localRandomSource = DevUrandomRandomSource()
+    print(localRandomSource.get_random_bytes(8))
+
+    # Init redis server
+    redisClient = redis.Redis(host=REDIS_SERVER['host'], port=REDIS_SERVER['port'], db=0)
+    redisClient.set('try', 'this is a try')
+    print(redisClient.get('try'))
