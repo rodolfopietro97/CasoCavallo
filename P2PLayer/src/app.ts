6f447be7cd95a2e30789bf8d163cc2aff50c2036
rodolfopietro97 <rodolfopietro97@gmail.com>
2021-09-08 19:31:28 +0200

    Hope all work remotely :D

---
+++ P2PLayer/src/app.ts
@@ -0,0 +1,122 @@
+/**
+ * Simple "server" instance
+ */
+
+import { createNode } from "./nodes";
+import { createWebNode } from "./webNode";
+const { fromString: uint8ArrayFromString } = require('uint8arrays/from-string')
+const { toString: uint8ArrayToString } = require('uint8arrays/to-string')
+
+
+const topic = 'news'
+
+
+const main = async () => {
+    // Init main arguments
+    let mainArguments : Array<String> = process.argv
+
+    // We need to pass listen mode or not (Client or Server)
+    console.assert(mainArguments.length == 3)
+
+    // Set listen mode (we are server that subscribe)
+    let listenMode : boolean = mainArguments[2] == '-s'
+
+    // Set ports on listen
+    let numberOfPorts = 10
+    let ports = Array.from(new Array(numberOfPorts), (x, i) => i)
+
+    let listenNodesTCP = ports.map((port : number) => {
+        return `/ip4/0.0.0.0/tcp/${4005 + port}`
+    })
+    let listenNodesWS = ports.map((port : number) => {
+        return `/ip4/127.0.0.1/tcp/${4005 + port + numberOfPorts}/ws`
+    })
+    let allAddresses =  [
+                '/ip4/0.0.0.0/tcp/4002',
+                '/ip4/127.0.0.1/tcp/4003/ws'
+            ].concat(listenNodesTCP).concat(listenNodesWS)
+
+
+    // Init node by passing listen addresses
+    let p2pNode = await createNode(
+        allAddresses
+    );
+
+    // Start node
+    await p2pNode.start();
+
+    // Listen on pubsub events
+    if(listenMode){
+        p2pNode.pubsub.on('fruit', (data) => {
+            console.log(`Received ${data.data} From ${data.receivedFrom}`)
+        })
+        p2pNode.pubsub.subscribe('fruit')
+    }
+
+    // Publish stuffs
+    if(!listenMode)
+        setInterval(()=>{
+            p2pNode.pubsub.publish('fruit', 'banana')
+        }, 200) 
+
+    // Starting log
+    console.log('listening on addresses:')
+    p2pNode.multiaddrs.forEach(addr => {
+        console.log(`${addr.toString()}/p2p/${p2pNode.peerId.toB58String()}`)
+    })
+
+    // When connection is established
+    p2pNode.connectionManager.on('peer:connect', (connection) => {
+        console.log('Connection established to:', connection.remotePeer.toB58String())	// Emitted when a new connection has been created
+    })
+
+    // When a new peer is discovered
+    p2pNode.on('peer:discovery', (peerId) => {
+        console.log('Discovered:', peerId.toB58String())
+    })
+}
+
+main();
+
+// const mainForBrowser = async () => {
+//     // Init node
+//     let p2pNode = await createWebNode(
+//         [
+//             '/ip4/127.0.0.1/tcp/8000/ws',
+//             '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star'
+//         ]
+//     );
+
+//     // Start node
+//     await p2pNode.start();
+
+//     const advertiseAddrs = p2pNode.multiaddrs
+//     console.log('libp2p is advertising the following addresses: ', advertiseAddrs)
+
+//     // // Listen on pubsub events
+//     // p2pNode.pubsub.on('fruit', (data) => {
+//     //     console.log(data)
+//     // })
+//     // p2pNode.pubsub.subscribe('fruit')
+
+//     // // Publish stuffs
+//     // p2pNode.pubsub.publish('fruit', new TextEncoder().encode('banana'))
+
+//     // // Starting log
+//     // console.log('listening on addresses:')
+//     // p2pNode.multiaddrs.forEach(addr => {
+//     //     console.log(`${addr.toString()}/p2p/${p2pNode.peerId.toB58String()}`)
+//     // })
+
+//     // // When connection is established
+//     // p2pNode.connectionManager.on('peer:connect', (connection) => {
+//     //     console.log('Connection established to:', connection.remotePeer.toB58String())	// Emitted when a new connection has been created
+//     // })
+
+//     // // When a new peer is discovered
+//     // p2pNode.on('peer:discovery', (peerId) => {
+//     //     console.log('Discovered:', peerId.toB58String())
+//     // })
+// }
+
+// mainForBrowser();
--- P2PLayer/src/nodes.ts
+++ P2PLayer/src/nodes.ts
@@ -1,13 +1,18 @@
 const Libp2p = require('libp2p')
+// @ts-ignore - no types
 const TCP = require('libp2p-tcp')
 const { NOISE } = require('libp2p-noise')
+// @ts-ignore - no types
 const MPLEX = require('libp2p-mplex')
 const Gossipsub = require('libp2p-gossipsub')
 const Bootstrap = require('libp2p-bootstrap')
 const PubsubPeerDiscovery = require('libp2p-pubsub-peer-discovery')
 const KadDHT = require('libp2p-kad-dht')
-
-const multiaddr = require('multiaddr')
+// @ts-ignore - no types
+const WS = require('libp2p-websockets')
+// @ts-ignore - no types
+const MulticastDNS = require('libp2p-mdns')
+const os = require('os')
 
 /**
  * Generate a P2P node
@@ -23,15 +28,36 @@ export async function createNode(ipAddresses: Array<string>) {
     // Listen addresses
     addresses: {
       listen: ipAddresses
+      // swarm: [
+      //   '/ip4/0.0.0.0/tcp/4002',
+      //   '/ip4/127.0.0.1/tcp/4003/ws'
+      // ],
+      // // snnounce: [],
+      // // noAnnounce: [],
+      // API: '/ip4/127.0.0.1/tcp/5002',
+      // gateway: '/ip4/127.0.0.1/tcp/9090',
+      // RPC: '/ip4/127.0.0.1/tcp/5003',
+      // delegates: [
+      //   '/dns4/node0.delegate.ipfs.io/tcp/443/https',
+      //   '/dns4/node1.delegate.ipfs.io/tcp/443/https',
+      //   '/dns4/node2.delegate.ipfs.io/tcp/443/https',
+      //   '/dns4/node3.delegate.ipfs.io/tcp/443/https'
+      // ]
+    },
+
+    dialer: {
+      maxParallelDials: 150, // 150 total parallel multiaddr dials
+      maxDialsPerPeer: 4, // Allow 4 multiaddrs to be dialed per peer in parallel
+      dialTimeout: 10e3 // 10 second dial timeout per peer dial
     },
 
     // Modules to use
     modules: {
-      transport: [TCP],
+      transport: [TCP, WS],
       connEncryption: [NOISE],
       streamMuxer: [MPLEX],
       pubsub: Gossipsub,
-      peerDiscovery: [Bootstrap],
+      peerDiscovery: [Bootstrap, MulticastDNS],
       dht: KadDHT
     },
 
@@ -39,6 +65,9 @@ export async function createNode(ipAddresses: Array<string>) {
     config: {
       peerDiscovery: {
         autoDial: true,
+        [MulticastDNS.tag]: {
+          enabled: true,
+        },
 
         [Bootstrap.tag]: {
           enabled: true,
@@ -48,8 +77,20 @@ export async function createNode(ipAddresses: Array<string>) {
             '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
             '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
             '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
-            '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
+            '/dns4/node0.preload.ipfs.io/tcp/443/wss/p2p/QmZMxNdpMkewiVZLMRxaNxUeZpDUb34pWjZ1kZvsd16Zic',
+            '/dns4/node1.preload.ipfs.io/tcp/443/wss/p2p/Qmbut9Ywz9YEDrz8ySBSgWyJk41Uvm2QJPhwDJzJyGFsD6',
+            '/dns4/node2.preload.ipfs.io/tcp/443/wss/p2p/QmV7gnbW5VTcJ3oyM2Xk1rdFBJ3kTkvxc87UFGsun29STS',
+            '/dns4/node3.preload.ipfs.io/tcp/443/wss/p2p/QmY7JB6MQXhxHvq7dBDh4HpbH29v4yE9JRadAVpndvzySN'
           ]
+          // list: [
+          //   '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
+          //   '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+          //   '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+          //   '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
+          //   '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+          //   '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+          // ]
         }
       },
       dht: {                        // The DHT options (and defaults) can be found in its documentation
@@ -57,17 +98,26 @@ export async function createNode(ipAddresses: Array<string>) {
         enabled: true,
         randomWalk: {
           enabled: true,            // Allows to disable discovery (enabled by default)
-          interval: 300e3,
-          timeout: 10e3
         }
       },
 
       pubsub: {                     // The pubsub options (and defaults) can be found in the pubsub router documentation
         enabled: true,
         emitSelf: true,             // whether the node should emit to self on publish
-        signMessages: true,         // if messages should be signed
-        strictSigning: true         // if message signing should be required
+      },
+
+      nat: {
+        enabled: true,
+        description: `ipfs@${os.hostname()}`
       }
+    },
+
+    metrics: {
+      enabled: true
+    },
+
+    peerStore: {
+      persistence: true
     }
   });
 
